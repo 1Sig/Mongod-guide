5e87da1f5eb6c0407c8b10ee174ee0103185bda3
Sigurd <Sigurdowre@gmail.com>
2024-05-21 19:04:50 +0200

    push

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Server statiske filer fra "public" katalogen
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rute for å serve HTML-filen
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start serveren
 app.listen(PORT, () => {
-    console.log(`Serveren kjører på http://localhost:${PORT}`);
+    console.log(`Serveren kjører på http://10.12.15.150:${PORT}`);
 });
